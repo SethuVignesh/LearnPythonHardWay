595ebf27f26ddeb2594bc398ebd9c183690ebf65
sethu <sethu.store@gmail.com>
2017-08-03 16:39:52 +0530

    modefied code
    trying with  “,“  operator

--- ex7.py
+++ ex7.py
@@ -1,6 +1,6 @@
 print "Mary has a littel lamb."
 print "Its fleece was white as %s."%'snow'
-print "And everywhere tjat MAry went."
+print "And everywhere that Mary went."
 print "."*10 #what'd that do?
 
 end1="C"
@@ -17,5 +17,4 @@ end11="e"
 end12="r"
 
 #watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4+ end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+print end1 + end2 + end3 + end4+ end5 + end6,end7 + end8 + end9 + end10 + end11 + end12
